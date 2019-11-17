bc91e5dda3135a22b37dd5a13a2842ea64df79d5
Michael Hart <M.HART@students.clark.edu>
2019-11-16 23:35:00 -0800

    Pushing early to get advice

---
+++ Module8/CA1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a guy named Mike");
+            Console.WriteLine("And he liked a girl");
+            Console.WriteLine("This girl lived 3000 miles away");
+            Console.WriteLine("But he liked her anyway, cuz he's an idiot");
+            Console.WriteLine("He made the girl mad, and now she won't talk to him");
+            Console.WriteLine("Several months later and he still likes her");
+            Console.WriteLine("Cuz he's an idiot");
+        }
+    }
+}
---
+++ Module8/CA2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("********** CA2 **********");
+            //while (args.Length != 0)
+            //{
+            //    string x = Console.ReadLine();
+            //    Console.WriteLine(x);
+            //}
+
+            //foreach (string s in args)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            if (args.Length == 0)
+                Console.WriteLine("No args");
+            else
+                foreach (string s in args)
+                {
+                    Console.WriteLine(s);
+                }
+        }
+    }
+}
